88703dddc6d45213408570730292ddad9e4e10bd
lizhongyi3 <lizhongyi3@xiaomi.com>
2021-12-04 17:01:45 +0800

    读取配置内容

--- src/main/java/com/itheima/controller/HelloControl.java
+++ src/main/java/com/itheima/controller/HelloControl.java
@@ -1,5 +1,8 @@
 package com.itheima.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +15,41 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping
 public class HelloControl {
+
+    @Value("${name}")
+    private String name;
+
+    @Value("${person.age}")
+    private int age;
+
+    @Value("${address[2]}")
+    private String city;
+
+    @RequestMapping("/getName")
+    public String getName(){
+        System.out.println(name);
+        return name;
+    }
+
+    @Autowired
+    private Environment env;
+
+
+    @RequestMapping("/getCity")
+    public String getCity(){
+        System.out.println(city);
+
+        System.out.println("----------------------------");
+        System.out.println(env.getProperty("address[2]"));
+        return city;
+    }
+
+    @RequestMapping("/getAge")
+    public int getAge(){
+        System.out.println(age);
+        return age;
+    }
+
     @RequestMapping("/hello")
     public String hello(){
         return "hello spring boot";
