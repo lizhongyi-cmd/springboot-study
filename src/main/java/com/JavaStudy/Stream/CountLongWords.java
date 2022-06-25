133c568f69990d7e822c6262e8b969b7806090c7
lzy <1198721761@qq.com>
2022-06-25 21:10:37 +0800

    stream

---
+++ src/main/java/com/JavaStudy/Stream/CountLongWords.java
@@ -0,0 +1,37 @@
+package com.JavaStudy.Stream;
+
+import com.sun.tools.classfile.StackMapTable_attribute;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Stream;
+
+/**
+ * @作者: lizhongyi
+ * @功能描述：
+ * @日期: 2022/6/25 5:20 下午
+ * @版本 1.0
+ */
+public class CountLongWords {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        List<String> words = new ArrayList<>();
+        words.add("sdfsdfsd");
+        words.add("sdfsdfsdjjjjjjg");
+        words.forEach(System.out::println);
+        long count = 0;
+        for (String w: words){
+            if(w.length()>12)
+                count++;
+        }
+        System.out.println(count);
+        long count2 = words.stream().filter(w -> w.length()>12).count();
+        System.out.println(count2);
+
+        words.stream().map(w -> w.toUpperCase()).forEach(System.out::println);
+
+        Stream<Double> randoms = Stream.generate(Math::random).limit(10);
+    }
+}
